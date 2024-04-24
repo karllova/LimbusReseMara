1272832a13f109e6775a03c5b50ef2217d9bc539
Hyunmyung Oh <linalan@kw.ac.kr>
2024-04-24 19:48:55 +0900

    init

---
+++ LimbusReseMara/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LimbusReseMara;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int TotalPoint { get; set; }
+    public Dictionary<string, int> stats;
+    private Dict dict = new Dict();
+    public int EgoNum = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+        TotalPoint = 0;
+        stats = new Dictionary<string, int>();
+        stats.Add("zeroTiers",0);
+        stats.Add("point5Tiers",0);
+        stats.Add("ThreeStars",0);
+        stats.Add("TwoStars",0);
+    }
+    public int GetTotalPoint()
+    {
+        return TotalPoint;
+    }
+
+    private void Box_Checked(object sender, RoutedEventArgs e)
+    {
+        CheckBox C = (CheckBox)sender;
+        string content = C.Content.ToString();
+        TotalPoint += dict.GetValue(content)[0];
+        if (dict.GetValue(content)[0]==50)
+        {
+            stats["zeroTiers"]++;
+        }
+        if (dict.GetValue(content)[0]==40)
+        {
+            stats["point5Tiers"]++;
+        }
+
+        if (dict.GetValue(content)[1] == 3)
+        {
+            stats["ThreeStars"]++;
+        }
+        else
+        {
+            stats["TwoStars"]++;
+        }
+    }
+
+    private void Box_Unchecked(object sender, RoutedEventArgs e)
+    {
+        CheckBox C = (CheckBox)sender;
+        string content = C.Content.ToString();
+        TotalPoint -= dict.GetValue(content)[0];
+        if (dict.GetValue(content)[0]==50)
+        {
+            stats["zeroTiers"]--;
+        }
+        if (dict.GetValue(content)[0]==40)
+        {
+            stats["point5Tiers"]--;
+        }
+
+        if (dict.GetValue(content)[1] == 3)
+        {
+            stats["ThreeStars"]--;
+        }
+        else
+        {
+            stats["TwoStars"]--;
+        }
+    }
+
+    private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        Window result = new Result();
+        result.Show();
+    }
+
+    private void ResetButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        UncheckAllCheckBoxes(this);
+        TotalPoint = 0;
+        foreach (var keys in stats.Keys)
+        {
+            stats[keys] = 0;
+        }
+    }
+    private void UncheckAllCheckBoxes(DependencyObject obj)
+    {
+        // ScrollViewer 안에 있는 모든 StackPanel을 찾습니다.
+        foreach (var child in FindVisualChildren<StackPanel>(obj))
+        {
+            // StackPanel 내의 모든 CheckBox를 찾습니다.
+            foreach (var checkBox in FindVisualChildren<CheckBox>(child))
+            {
+                // CheckBox를 해제합니다.
+                checkBox.IsChecked = false;
+            }
+        }
+    }
+
+    private IEnumerable<T> FindVisualChildren<T>(DependencyObject obj) where T : DependencyObject
+    {
+        if (obj != null)
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                {
+                    yield return (T)child;
+                }
+
+                foreach (T childOfChild in FindVisualChildren<T>(child))
+                {
+                    yield return childOfChild;
+                }
+            }
+        }
+    }
+}
---
+++ LimbusReseMara/Result.xaml.cs
@@ -0,0 +1,50 @@
+using System.Windows;
+
+namespace LimbusReseMara;
+
+public partial class Result : Window
+{
+    public Result()
+    {
+        InitializeComponent();
+        int totalpoint = ((MainWindow)Application.Current.MainWindow).GetTotalPoint();
+        this.TotalPointText.Text = totalpoint.ToString();
+        this.Comment.Text = comments(totalpoint);
+        var stats = ((MainWindow)Application.Current.MainWindow).stats;
+        this.zeroTiers.Text = stats["zeroTiers"].ToString();
+        this.point5Tiers.Text = stats["point5Tiers"].ToString();
+        this.ThreeStars.Text = stats["ThreeStars"].ToString();
+        this.TwoStars.Text = stats["TwoStars"].ToString();
+    }
+    private string comments(int point)
+    {
+        if (point>120)
+        {
+            return "무조건 이륙";
+        }
+        if (point>=100)
+        {
+            return "이륙 가능 점수";
+        }
+        if (point>=80)
+        {
+            return "0.5티어 인격 존재 시, 이륙 가능";
+        }
+        if (point>=70)
+        {
+            return "0티어 인격 존재 시, 이륙 가능.";
+        }
+
+        if (point>=50)
+        {
+            return "이륙불가";
+        }
+        if (point>=40)
+        {
+            return "진심?";
+        }
+
+        return "썩은내상어";
+    }
+    
+}
